a97d589fe5d5ea215e623c674c8899d83851cbf0
biamaxine <2023005718@ifam.edu.br>
2024-11-08 13:09:52 -0400

    feat: UPDATE

--- lista_selecao/exercise_13.c
+++ lista_selecao/exercise_13.c
@@ -5,13 +5,12 @@ int main() {
 
   scanf("%f", &pollution_index);
 
-  if (pollution_index >= 0.3 && pollution_index < 0.4) {
+  if (pollution_index >= 0.3 && pollution_index < 0.4)
     printf("As industrias do 1º grupo devem ser intimadas a suspender suas atividades.\n");
-  } else if (pollution_index >= 0.4 && pollution_index < 0.5) {
+  else if (pollution_index >= 0.4 && pollution_index < 0.5)
     printf("As industrias do 1º e 2º grupo devem ser intimadas a suspender suas atividades.\n");
-  } else if (pollution_index >= 0.5) {
+  else
     printf("As industrias dos 3 grupos devem ser intimadas a suspender suas atividades.\n");
-  }
 
   return 0;
 }
